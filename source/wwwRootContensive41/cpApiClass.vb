9abdf8d142b0c8958318e722280dc5e2e03e597d
jay <jay@contensive.com>
2017-08-26 13:08:55 -0400

    added plus and % to characters encoded from ASPX

--- source/wwwRootContensive41/cpApiClass.vb
+++ source/wwwRootContensive41/cpApiClass.vb
@@ -183,7 +183,7 @@ Public Class cpApiClass
     End Function
     '
     Private Shared Function encodeAmpEqual(source As String) As String
-        Return source.Replace("&", "%3D").Replace("=", "%26")
+        Return source.Replace("%", "%25").Replace("&", "%3D").Replace("=", "%26").Replace("+", "%2B")
     End Function
 
 End Class
