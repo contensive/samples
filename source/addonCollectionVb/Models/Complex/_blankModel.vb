a4bedf4f00f9a17f6ca8d6d7beae614116d195dd
jay <jay@contensive.com>
2018-01-26 18:44:35 -0500

    adding complex model pattern to VB for sql createList

---
+++ source/addonCollectionVb/Models/Complex/_blankModel.vb
@@ -0,0 +1,30 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports Contensive.BaseClasses
+
+Namespace Contensive.Addons.xxxxxCollectionNameSpaceGoesHerexxxxx.Models.Complex     '<------ set namespace
+    Public Class xxxxxmodelNameGoesHerexxxxx        '<------ set set model Name and everywhere that matches this string
+        Inherits baseComplexModel
+        Implements ICloneable
+        '
+        '====================================================================================================
+        '-- const
+        Private Shadows Const contentDataSource As String = "default"             '<------ set to datasource if not default
+        '
+        '====================================================================================================
+        ' -- instance properties
+        Public Property samplePropertyFromQuery As String
+        '
+        '====================================================================================================
+        Public Overloads Shared Function createList(cp As CPBaseClass, organizationId As Integer, Optional pageSize As Integer = 999999, Optional pageNumber As Integer = 1) As List(Of accreditationExportRawModel)
+            Dim sql As String = My.Resources.ExportAccreditationSql.Replace("{0}", organizationId.ToString())
+            Return createListFromSql(Of accreditationExportRawModel)(cp, sql, pageSize, pageNumber)
+        End Function
+
+    End Class
+End Namespace
---
+++ source/addonCollectionVb/Models/DbModels/MemberTopicRuleModel.vb
@@ -0,0 +1,109 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports Contensive.BaseClasses
+
+Namespace Contensive.Addons.xxxxxCollectionNameSpaceGoesHerexxxxx.Models.DbModels     '<------ set namespace
+    Public Class MemberTopicRuleModel        '<------ set set model Name and everywhere that matches this string
+        Inherits baseModel
+        Implements ICloneable
+        '
+        '====================================================================================================
+        '-- const
+        Public Const contentName As String = "Member Topic Rules"      '<------ set content name
+        Public Const contentTableName As String = "ccMemberTopicRules"   '<------ set to tablename for the primary content (used for cache names)
+        Private Shadows Const contentDataSource As String = "default"             '<------ set to datasource if not default
+        '
+        '====================================================================================================
+        ' -- instance properties
+
+        Public Property MemberID As Integer
+        Public Property TopicID As Integer
+        '
+        '====================================================================================================
+        Public Overloads Shared Function add(cp As CPBaseClass) As MemberTopicRuleModel
+            Return add(Of MemberTopicRuleModel)(cp)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function create(cp As CPBaseClass, recordId As Integer) As MemberTopicRuleModel
+            Return create(Of MemberTopicRuleModel)(cp, recordId)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function create(cp As CPBaseClass, recordGuid As String) As MemberTopicRuleModel
+            Return create(Of MemberTopicRuleModel)(cp, recordGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function createByName(cp As CPBaseClass, recordName As String) As MemberTopicRuleModel
+            Return createByName(Of MemberTopicRuleModel)(cp, recordName)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Sub save(cp As CPBaseClass)
+            MyBase.save(cp)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Sub delete(cp As CPBaseClass, recordId As Integer)
+            delete(Of MemberTopicRuleModel)(cp, recordId)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Sub delete(cp As CPBaseClass, ccGuid As String)
+            delete(Of MemberTopicRuleModel)(cp, ccGuid)
+        End Sub
+        '
+        '====================================================================================================
+        Public Overloads Shared Function createList(cp As CPBaseClass, sqlCriteria As String, Optional sqlOrderBy As String = "id") As List(Of MemberTopicRuleModel)
+            Return createList(Of MemberTopicRuleModel)(cp, sqlCriteria, sqlOrderBy)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordName(cp As CPBaseClass, recordId As Integer) As String
+            Return baseModel.getRecordName(Of MemberTopicRuleModel)(cp, recordId)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordName(cp As CPBaseClass, ccGuid As String) As String
+            Return baseModel.getRecordName(Of MemberTopicRuleModel)(cp, ccGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getRecordId(cp As CPBaseClass, ccGuid As String) As Integer
+            Return baseModel.getRecordId(Of MemberTopicRuleModel)(cp, ccGuid)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Shared Function getCount(cp As CPBaseClass, sqlCriteria As String) As Integer
+            Return baseModel.getCount(Of MemberTopicRuleModel)(cp, sqlCriteria)
+        End Function
+        '
+        '====================================================================================================
+        Public Overloads Function getUploadPath(fieldName As String) As String
+            Return MyBase.getUploadPath(Of MemberTopicRuleModel)(fieldName)
+        End Function
+        '
+        '====================================================================================================
+        '
+        Public Function Clone(cp As CPBaseClass) As MemberTopicRuleModel
+            Dim result As MemberTopicRuleModel = DirectCast(Me.Clone(), MemberTopicRuleModel)
+            result.id = cp.Content.AddRecord(contentName)
+            result.ccguid = cp.Utils.CreateGuid()
+            result.save(cp)
+            Return result
+        End Function
+        '
+        '====================================================================================================
+        '
+        Public Function Clone() As Object Implements ICloneable.Clone
+            Return Me.MemberwiseClone()
+        End Function
+
+    End Class
+End Namespace
