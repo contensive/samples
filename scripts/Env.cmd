0eaabf119154e9dfcdb8e2b6a5fed66ce91eff46
jay <jay@contensive.com>
2019-09-19 14:32:09 -0400

    minor build script updates

---
+++ scripts/Env.cmd
@@ -0,0 +1,12 @@
+
+rem all paths are relative to the git scripts folder
+
+set appName=designblocks
+set majorVersion=5
+set minorVersion=1
+set collectionName=aoToolPanel
+set solutionName=aoToolPanel.sln
+set collectionPath=..\collections\aoToolPanel\
+set binPath=..\source\aoToolPanel\bin\debug\
+set msbuildLocation=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\
+set deploymentFolderRoot=C:\Users\jay\Desktop\deployments\aoToolPanel\Dev\
